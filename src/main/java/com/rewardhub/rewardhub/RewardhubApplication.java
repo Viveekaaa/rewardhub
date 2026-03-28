bb960041b20d1806acee7e833b9e25eaa138384b
Vivek H A <13082001vivekreddy@gmail.com>
2026-03-28 16:32:14 +0530

    Initial project setup with Spring Boot 4.0.5

---
+++ src/main/java/com/rewardhub/rewardhub/RewardhubApplication.java
@@ -0,0 +1,13 @@
+package com.rewardhub.rewardhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rewardhub/rewardhub/RewardhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rewardhub.rewardhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
